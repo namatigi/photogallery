a46348372f2f94527b5f8f11a5d9e2bae055ae04
namatigi <leon.mangu@gmail.com>
2017-07-14 23:20:42 +0300

    combining the object class and the user class methods and properties

--- includes/user.php
+++ includes/user.php
@@ -25,6 +25,86 @@ class User extends DatabaseObject {
 
 
 
+    public static function find_all(){
+        global $database;
+        //        $result_set = $database->query("SELECT * FROM users");
+        return $this->find_by_sql("SELECT * FROM ".self::$table_name);
+
+    }
+
+    public static function find_by_id($id=0){
+        global $database;
+        //      $result_set = $database->query("SELECT * FROM users WHERE id={$id}");
+        $result_array= $this->find_by_sql("SELECT * FROM ".self::$table_name. " WHERE id={$id} LIMIT 1");
+
+        return !empty($result_array)?array_shift($result_array):false;
+
+    }
+
+    public static function find_by_sql($sql=""){
+        global $database;
+        $result_set = $database->query($sql);
+        $object_array = array();
+        while($row=$database->fetch_array($result_set)){
+            $object_array[]=$this->instantiate($row);
+        }
+        return $object_array;
+    }
+
+    private static function instantiate($record){
+        //simple, long form.
+        // $class_name = get_called_class();
+        $object = new self();
+        //        $object = new self();
+        //        $object->id= $record['id'];
+        //        $object->username=$record['username'];
+        //        $object->password=$record['password'];
+        //        $object->first_name=$record['first_name'];
+        //        $object->last_name=$record['last_name'];
+        //        return $object;
+
+        foreach ($record as $attribute=>$value){
+            if($object->has_attribute($attribute)){
+                $object->$attribute=$value;
+            }
+        }
+        return $object;
+
+    }
+
+    protected function attributes(){
+      //        return get_object_vars($this);
+
+        $attributes =array();
+        foreach (self::$db_fields as $field){
+            if(property_exists($this,$field)){
+                $attributes[$field]=$this->$field;
+            }
+        }
+        return $attributes;
+    }
+
+    private function has_attribute($attribute){
+      //        $object_vars = get_object_vars($this);
+
+          $object_vars = $this->attributes();
+
+        return array_key_exists($attribute,$object_vars);
+    }
+
+    protected function sanitized_attributes(){
+        global $database;
+
+        $clean_attributes = array();
+
+        foreach ($this->attributes() as $key => $value){
+            $clean_attributes[$key]=$database->escape_value($value);
+        }
+
+        return $clean_attributes;
+    }
+
+
     public static function authenticate($username="",$password=""){
         global $database;
 
